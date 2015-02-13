f2c5d1d82d3c8d0d9726a44e0e4d4d480db7a034
Michael Drogalis <mjd3089@rit.edu>
2015-02-12 17:38:14 -0800

    Start ZK

--- chef/ec2/recipes/metrics.rb
+++ chef/ec2/recipes/metrics.rb
@@ -1,2 +1,9 @@
 include_recipe "apt::default"
 
+apt_package "graphite-web" do
+  action :install
+end
+
+execute "DEBIAN_FRONTEND=noninteractive apt-get -q -y --force-yes install graphite-carbon"
+execute "graphite-manage syncdb"
+
--- chef/ec2/recipes/zk.rb
+++ chef/ec2/recipes/zk.rb
@@ -1,1 +1,3 @@
 include_recipe "zookeeper::default"
+
+execute "/opt/zookeeper/zookeeper-3.4.6/bin/zkServer.sh start"
