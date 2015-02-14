186f5c098eafbbd40964fe9600abc8fbdc2db6f1
Michael Drogalis <mjd3089@rit.edu>
2015-02-14 09:26:49 -0800

    Packages.

--- chef/ec2/recipes/peer.rb
+++ chef/ec2/recipes/peer.rb
@@ -4,6 +4,38 @@ include_recipe "git::default"
 include_recipe "lein::default"
 include_recipe "collectd::default"
 
+apt_package "automake" do
+  action :install
+end
+
+apt_package "libtool" do
+  action :install
+end
+
+apt_package "libbz2-dev" do
+  action :install
+end
+
+apt_package "libpq-dev" do
+  action :install
+end
+
+apt_package "libgeos++-dev" do
+  action :install
+end
+
+apt_package "libxml2-dev" do
+  action :install
+end
+
+apt_package "build-essential" do
+  action :install
+end
+
+apt_package "protobuf-c-compiler" do
+  action :install
+end
+
 apt_package "ibprotobuf-c0-dev" do
   action :install
 end
