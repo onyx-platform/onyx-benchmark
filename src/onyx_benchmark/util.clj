718c6815cd1332c4c2cff2b7515b5a50966d6d56
Lucas Bradstreet <lucasbradstreet@gmail.com>
2015-02-22 01:35:47 +0800

    Ensure instance is running.

--- src/onyx_benchmark/util.clj
+++ src/onyx_benchmark/util.clj
@@ -16,15 +16,20 @@
 (defn metrics? [tags]
   (seq (filter (fn [t] (and (= (:Key t) "stack-role") (= (:Value t) "metrics"))) tags)))
 
+(defn running? [i]
+  (= "running" (:Name (:State i))))
+
 (defn -main [stack-name stack-region file & args]
   (let [contents (parse-string (slurp file) true)
         instances (mapcat :Instances (:Reservations contents))]
-    (spit "/home/ubuntu/zookeeper.txt" (:PublicDnsName (first (filter (fn [i] (and (zk? (:Tags i))
-                                                                                  (in-stack? stack-name (:Tags i))
-                                                                                  (in-region? stack-region (:Tags i))))
+    (spit "/home/ubuntu/zookeeper.txt" (:PublicDnsName (first (filter (fn [i] (and (running? i)
+                                                                                   (zk? (:Tags i))
+                                                                                   (in-stack? stack-name (:Tags i))
+                                                                                   (in-region? stack-region (:Tags i))))
                                                                       instances))))
-    (spit "/home/ubuntu/metrics.txt" (:PublicDnsName (first (filter (fn [i] (and (metrics? (:Tags i))
-                                                                                (in-stack? stack-name (:Tags i))
-                                                                                (in-region? stack-region (:Tags i))))
+    (spit "/home/ubuntu/metrics.txt" (:PublicDnsName (first (filter (fn [i] (and (running? i)
+                                                                                 (metrics? (:Tags i))
+                                                                                 (in-stack? stack-name (:Tags i))
+                                                                                 (in-region? stack-region (:Tags i))))
                                                                     instances))))))
 
