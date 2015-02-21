59907d3e118c46facaaae21d3095fe45f5b6663c
Michael Drogalis <mjd3089@rit.edu>
2015-02-20 18:01:31 -0800

    Filter by region and stack name.

--- src/onyx_benchmark/util.clj
+++ src/onyx_benchmark/util.clj
@@ -1,6 +1,12 @@
 (ns onyx-benchmark.util
   (:require [cheshire.core :refer [parse-string]]))
 
+(defn in-stack? [stack-name tags]
+  (seq (filter (fn [t] (and (= (:Key t) "stack-name") (= (:Value t) stack-name))) tags)))
+
+(defn in-region? [stack-region tags]
+  (seq (filter (fn [t] (and (= (:Key t) "stack-region") (= (:Value t) stack-region))) tags)))
+
 (defn peer? [tags]
   (seq (filter (fn [t] (and (= (:Key t) "stack-role") (= (:Value t) "peer"))) tags)))
 
@@ -10,9 +16,15 @@
 (defn metrics? [tags]
   (seq (filter (fn [t] (and (= (:Key t) "stack-role") (= (:Value t) "metrics"))) tags)))
 
-(defn -main [file & args]
+(defn -main [stack-name stack-region file & args]
   (let [contents (parse-string (slurp file) true)
         instances (mapcat :Instances (:Reservations contents))]
-    (spit "/home/ubuntu/zookeeper.txt" (:PublicDnsName (first (filter (fn [i] (zk? (:Tags i))) instances))))
-    (spit "/home/ubuntu/metrics.txt" (:PublicDnsName (first (filter (fn [i] (metrics? (:Tags i))) instances))))))
+    (spit "/home/ubuntu/zookeeper.txt" (:PublicDnsName (first (filter (fn [i] (and (zk? (:Tags i))
+                                                                                  (in-stack? stack-name (:Tags i))
+                                                                                  (in-region? stack-region (:Tags i))))
+                                                                      instances))))
+    (spit "/home/ubuntu/metrics.txt" (:PublicDnsName (first (filter (fn [i] (and (metrics? (:Tags i))
+                                                                                (in-stack? stack-name (:Tags i))
+                                                                                (in-region? stack-region (:Tags i))))
+                                                                    instances))))))
 
