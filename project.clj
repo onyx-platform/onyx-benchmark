573abe7d18ee450528caf538e6c5e5430b84e128
Lucas Bradstreet <lucasbradstreet@gmail.com>
2015-05-01 20:55:39 +0800

    Use 1.7 clojure as this is what most people will use in the future
    Also warn on reflection and turn off assertions.

--- project.clj
+++ project.clj
@@ -2,10 +2,11 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   ;"-Daeron.rcv.buffer.length=100000" "-Daeron.socket.so_sndbuf=320000" "-Daeron.socket.so_rcvbuf=320000"  "-Daeron.term.buffer.length=131072" "-Daeron.rcv.initial.window.length=131072"]
-  :java-opts ["-server" "-Xmx2g" ]
+  :java-opts ["-server" "-Xmx2g"]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :global-vars  {*warn-on-reflection* true *assert* false}
+  :dependencies [[org.clojure/clojure "1.7.0-beta2"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [io.netty/netty-all "4.0.25.Final"]
                  [org.clojure/data.fressian "0.2.0"]
