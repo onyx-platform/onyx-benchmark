3a5877119b4c159cfcac9c8c26fafcc6fdf9b269
Michael Drogalis <mjd3089@rit.edu>
2015-05-09 09:43:10 -0700

    Accept terms.

--- chef/ec2/attributes/default.rb
+++ chef/ec2/attributes/default.rb
@@ -1,5 +1,6 @@
 node.default['java']['jdk_version'] = 8
 node.default['java']['install_flavor'] = 'oracle'
+node.default['java']['oracle']['accept_oracle_download_terms'] = true
 
 node.default['lein']['version'] = '2.5.1'
 node.default['lein']['user'] = 'ubuntu'
