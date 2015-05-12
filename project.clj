f5a54fe82c64a20c39ad45ceefdb82749fd35918
Lucas Bradstreet <lucasbradstreet@gmail.com>
2015-05-12 16:13:13 +0800

    timbre appender brings in incompatible version of encore

--- project.clj
+++ project.clj
@@ -2,14 +2,19 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   ;"-Daeron.rcv.buffer.length=100000" "-Daeron.socket.so_sndbuf=320000" "-Daeron.socket.so_rcvbuf=320000"  "-Daeron.term.buffer.length=131072" "-Daeron.rcv.initial.window.length=131072"]
-  :java-opts ["-server" "-Xmx2g"]
+  :java-opts ["-server" "-Xmx2g"
+              ;"-XX:+UseG1GC"
+              "-XX:+UnlockCommercialFeatures" "-XX:+FlightRecorder" "-XX:StartFlightRecording=duration=240s,filename=myrecording.jfr"
+              ]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :global-vars  {*warn-on-reflection* true *assert* false}
+  :global-vars  {*warn-on-reflection* true *assert* false
+                 ;*unchecked-math* :warn-on-boxed
+                 }
   :dependencies [[org.clojure/clojure "1.7.0-beta2"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [io.netty/netty-all "4.0.25.Final"]
-                 [yeller-timbre-appender "0.4.1"]
+                 [yeller-timbre-appender "0.4.1" :exclusions [com.taoensso/timbre]]
                  [org.clojure/data.fressian "0.2.0"]
                  [com.mdrogalis/onyx "0.6.0-SNAPSHOT"]
                  [riemann-clojure-client "0.3.2" :exclusions [io.netty/netty]]
