62f41d490dcbd7feff387060b18aacfd9864a9d0
Lucas Bradstreet <lucasbradstreet@gmail.com>
2015-05-14 17:49:07 +0800

    Bump up RAM.

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   ;"-Daeron.rcv.buffer.length=100000" "-Daeron.socket.so_sndbuf=320000" "-Daeron.socket.so_rcvbuf=320000"  "-Daeron.term.buffer.length=131072" "-Daeron.rcv.initial.window.length=131072"]
-  :java-opts ["-server" "-Xmx2g"
+  :java-opts ["-server" "-Xmx3g"
               ;"-XX:+UseG1GC"
               "-XX:+UnlockCommercialFeatures" "-XX:+FlightRecorder" "-XX:StartFlightRecording=duration=240s,filename=myrecording.jfr"
               ]
