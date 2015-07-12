487d6b09483a8aaec675eb948a717a21c065e336
Lucas Bradstreet <lucas.bradstreet@onyxplatform.org>
2015-07-12 21:37:31 +0200

    Use 0.7.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -2,9 +2,9 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   ;"-Daeron.rcv.buffer.length=100000" "-Daeron.socket.so_sndbuf=320000" "-Daeron.socket.so_rcvbuf=320000"  "-Daeron.term.buffer.length=131072" "-Daeron.rcv.initial.window.length=131072"]
-  :java-opts ^:replace ["-server" "-Xmx2g"
+  :java-opts ^:replace ["-server" "-Xmx8g"
                         ;"-XX:+UseG1GC"
-                        ;"-XX:+UnlockCommercialFeatures" "-XX:+FlightRecorder" "-XX:StartFlightRecording=duration=240s,filename=myrecording.jfr "-XX:+UnlockDiagnosticVMOptions" 
+                        ;"-XX:+UnlockCommercialFeatures" "-XX:+FlightRecorder" "-XX:StartFlightRecording=duration=240s,filename=myrecording.jfr" "-XX:+UnlockDiagnosticVMOptions" 
                         ; "-XX:+TraceClassLoading" "-XX:+LogCompilation" "-XX:+PrintAssembly"
                         ]
 
@@ -18,6 +18,6 @@
                  [interval-metrics "1.0.0"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [io.netty/netty-all "4.0.25.Final"]
-                 [org.onyxplatform/onyx "0.6.1-SNAPSHOT"]
+                 [org.onyxplatform/onyx "0.7.0-SNAPSHOT"]
                  [riemann-clojure-client "0.3.2" :exclusions [io.netty/netty]]
                  [cheshire "5.4.0"]])
