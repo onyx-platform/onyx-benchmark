450ad283a7068fa8f9e13c19882a245800b161de
Lucas Bradstreet <lucas.bradstreet@onyxplatform.org>
2015-07-16 00:50:38 +0200

    Reduce subscriber count back down to 2

--- src/onyx_benchmark/peer.clj
+++ src/onyx_benchmark/peer.clj
@@ -90,7 +90,7 @@
                      :onyx/id id
                      :onyx.messaging/bind-addr (slurp "http://169.254.169.254/latest/meta-data/local-ipv4")
                      :onyx.messaging/peer-ports (vec (range 40000 40200))
-                     :onyx.messaging.aeron/subscriber-count 4
+                     :onyx.messaging.aeron/subscriber-count 2
                      :onyx.peer/join-failure-back-off 500
                      :onyx.peer/job-scheduler :onyx.job-scheduler/greedy
                      :onyx.messaging/impl (keyword messaging)}
