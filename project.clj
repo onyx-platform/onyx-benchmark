ec6a4a7e64e6669e88026ad1d1d21dcf31269663
Lucas Bradstreet <lucasbradstreet@gmail.com>
2015-11-26 19:11:39 +0800

    Bump metrics dependency

--- project.clj
+++ project.clj
@@ -17,6 +17,22 @@
                         ; "-XX:+TraceClassLoading" "-XX:+LogCompilation" "-XX:+PrintAssembly"
                         ]
 
+  :profiles {:bench {:dependencies [[org.onyxplatform/onyx "0.0.0-BENCH"]]
+                     :java-opts ^:replace ["-server" 
+                                           "-Xmx6g"
+                                           "-XX:BiasedLockingStartupDelay=0" 
+                                           "-Daeron.mtu.length=16384" 
+                                           "-Daeron.socket.so_sndbuf=2097152" 
+                                           "-Daeron.socket.so_rcvbuf=2097152" 
+                                           "-Daeron.rcv.buffer.length=16384" 
+                                           "-Daeron.rcv.initial.window.length=2097152" 
+                                           "-Dagrona.disable.bounds.checks=true"
+
+                                           "-XX:+UnlockCommercialFeatures" "-XX:+FlightRecorder" 
+                                           "-XX:+UnlockDiagnosticVMOptions" 
+                                           "-XX:StartFlightRecording=duration=1080s,filename=localrecording.jfr"]}}
+
+
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :global-vars  {*warn-on-reflection* true 
@@ -25,7 +41,7 @@
   :java-source-paths ["src/java"]
   :dependencies [[org.clojure/clojure "1.7.0"]
 		 [org.onyxplatform/onyx "0.8.1-SNAPSHOT"]
-		 [org.onyxplatform/onyx-metrics "0.8.0.4-20151117.104014-7"]
+		 [org.onyxplatform/onyx-metrics "0.8.1.0-alpha9"]
 		 [riemann-clojure-client "0.4.1"]
-                 [danlentz/clj-uuid "0.1.6"]
+                 [gnuplot "0.1.0"]
 		 [cheshire "5.4.0"]])
