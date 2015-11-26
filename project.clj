c59b6e327357a56b307b671ad797cb110da83a5e
Lucas Bradstreet <lucasbradstreet@gmail.com>
2015-11-26 23:28:01 +0800

    Bump metrics

--- project.clj
+++ project.clj
@@ -41,7 +41,7 @@
   :java-source-paths ["src/java"]
   :dependencies [[org.clojure/clojure "1.7.0"]
 		 [org.onyxplatform/onyx "0.8.1-SNAPSHOT"]
-		 [org.onyxplatform/onyx-metrics "0.8.1.0-20151126.151419-11"]
+		 [org.onyxplatform/onyx-metrics "0.8.1.0-20151126.152432-12"]
 		 [riemann-clojure-client "0.4.1"]
                  [gnuplot "0.1.0"]
 		 [cheshire "5.4.0"]])
