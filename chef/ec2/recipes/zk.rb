b59c9c8a02d396fa760e730356708a2aece56fab
Michael Drogalis <mjd3089@rit.edu>
2015-11-28 07:30:49 -0800

    Ansible version of benchmark.

--- chef/ec2/recipes/zk.rb
+++
@@ -1,9 +0,0 @@
-include_recipe "apt::default"
-include_recipe "java::default"
-include_recipe "zookeeper::default"
-
-execute "/opt/zookeeper/zookeeper-3.4.6/bin/zkServer.sh start"
-
-execute "apt-get install -y python-pip"
-execute "pip install https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-latest.tar.gz"
-
