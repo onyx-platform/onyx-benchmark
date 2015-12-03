357cad149e58d60c39b8b77bde7e197fb4e9df9c
Lucas Bradstreet <lucasbradstreet@gmail.com>
2015-12-04 04:05:59 +0800

    Parameterize aeron poll strategies

--- src/onyx_benchmark/peer.clj
+++ src/onyx_benchmark/peer.clj
@@ -31,7 +31,7 @@
 
 (defn restartable? [e] 
   true)
-(defn -main [zk-addr riemann-addr riemann-port id n-peers subscriber-count messaging & args]
+(defn -main [zk-addr riemann-addr riemann-port id n-peers offer-idle-strategy poll-idle-strategy subscriber-count messaging & args]
   (let [local? (= zk-addr "128.0.0.1:2189")
 
         env-config {:onyx.bookkeeper/server? true
@@ -47,8 +47,8 @@
                                                  (slurp "http://169.254.169.254/latest/meta-data/local-ipv4")) 
                      :onyx.messaging/peer-port 40000
                      :onyx.messaging.aeron/write-buffer-size 200000
-                     :onyx.messaging.aeron/offer-idle-strategy :low-restart-latency
-                     :onyx.messaging.aeron/poll-idle-strategy :low-restart-latency
+                     :onyx.messaging.aeron/offer-idle-strategy (keyword offer-idle-strategy)
+                     :onyx.messaging.aeron/poll-idle-strategy (keyword poll-idle-strategy)
                      :onyx.messaging.aeron/embedded-driver? false
                      :onyx.messaging.aeron/subscriber-count (Integer/parseInt subscriber-count)
                      ;; more accurate benching locally
