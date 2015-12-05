9e66857e699196f939e05b9c83bc225d652ef6e7
Lucas Bradstreet <lucasbradstreet@gmail.com>
2015-12-05 23:52:09 +0800

    Bump metrics

--- project.clj
+++ project.clj
@@ -42,7 +42,7 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
 		 ;[org.onyxplatform/onyx "0.8.2"]
 		 [org.onyxplatform/onyx "0.8.3-SNAPSHOT"]
-		 [org.onyxplatform/onyx-metrics "0.8.2.8-20151205.133524-16"]
+		 [org.onyxplatform/onyx-metrics "0.8.2.8-20151205.155143-17"]
 		 [riemann-clojure-client "0.4.1"]
                  [gnuplot "0.1.0"]
 		 [cheshire "5.4.0"]])
