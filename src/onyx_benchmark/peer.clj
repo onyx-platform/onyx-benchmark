392773b371b793790b52990b10151e12882f1aa5
Michael Drogalis <mjd3089@rit.edu>
2016-01-04 18:22:37 -0800

    Moves more stuff into external peer file.

--- src/onyx_benchmark/peer.clj
+++ src/onyx_benchmark/peer.clj
@@ -33,32 +33,27 @@
   true)
 
 (defn -main [zk-addr peer-config-file riemann-addr riemann-port id n-peers & args]
-  (let [local? (= zk-addr "127.0.0.1:2189")
-
-        env-config {:onyx.bookkeeper/server? true
-                    :onyx/id id
+  (let [env-config {:onyx/id id
+                    :onyx.bookkeeper/server? true
+                    :onyx.bookkeeper/local-quorum? (= zk-addr "127.0.0.1:2189")
                     :zookeeper/address zk-addr
-                    :onyx.bookkeeper/local-quorum? local?
                     :zookeeper/server? false}
 
         peer-cfg (read-string (slurp peer-config-file))
 
         peer-config (merge
                      {:onyx.messaging/bind-addr
-                      (if local?
-                        "127.0.0.1"
-                        (slurp "http://169.254.169.254/latest/meta-data/local-ipv4"))}
+                      (slurp "http://169.254.169.254/latest/meta-data/local-ipv4")}
                      peer-cfg)
 
         n-peers-parsed (Integer/parseInt n-peers)
         peer-group (onyx.api/start-peer-group peer-config)
         env (onyx.api/start-env env-config)
 
-        host-id (str (java.util.UUID/randomUUID))
         m-cfg (monitoring/monitoring-config 10000)
         riemann-config {:riemann/address riemann-addr 
                         :riemann/batch-size 1
                         :riemann/port (Integer/parseInt riemann-port)} 
         monitoring-thread (riemann/riemann-sender riemann-config (:monitoring/ch m-cfg))
         peers (onyx.api/start-peers n-peers-parsed peer-group m-cfg)]
-    (<!! (chan))))
+    @(promise)))
